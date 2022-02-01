c1977194ab0ece82d865b5e6e2e02c4cec8f4374
Erick Vaghi <vaghi.erick@gmail.com>
2022-02-01 17:27:01 +0100

    SphereCast added to ground check

--- Assets/Scripts/Erick Vaghi/PlayerController.cs
+++ Assets/Scripts/Erick Vaghi/PlayerController.cs
@@ -9,6 +9,7 @@ public class PlayerController : MonoBehaviour
     public float moveSpeed = 5f;
     public float jumpForce = 500f;
     public float groundCheckDistance = 1f;
+    public float groundCheckSphereRadius = 0.5f;
     
     // Update is called once per frame
     void Update()
@@ -23,8 +24,16 @@ public class PlayerController : MonoBehaviour
         var jumpInput = Input.GetKeyDown(KeyCode.Space);
         
         //check if we're grounded, using a raycast
-        var isGrounded = Physics.Raycast(transform.position, Vector3.down, groundCheckDistance);
+        //Vector3.down: down in world space.
+        //-transform.up: down in the GameObject's local space.
+        // var isGrounded = Physics.Raycast(transform.position, Vector3.down, groundCheckDistance);
         
+        Ray sphereCastRay = new Ray(transform.position, Vector3.down);
+        var isGrounded = Physics.SphereCast(sphereCastRay, groundCheckSphereRadius, groundCheckDistance);
+        
+        //Draw a ray in the editor, only for visualization.
+        Debug.DrawRay(transform.position, Vector3.down * groundCheckDistance, Color.cyan);
+
         //If we pressed the jump button: then jump
         if (jumpInput && isGrounded)
             myRigidBody.AddForce(0, jumpForce, 0);
