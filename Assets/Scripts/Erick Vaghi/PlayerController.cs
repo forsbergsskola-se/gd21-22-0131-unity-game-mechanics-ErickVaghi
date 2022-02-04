393aa81d74374d9eaf331bd45865df43375b142e
Erick Vaghi <vaghi.erick@gmail.com>
2022-02-04 16:49:22 +0100

    Separate Methods in Player controller

--- Assets/Scripts/Erick Vaghi/PlayerController.cs
+++ Assets/Scripts/Erick Vaghi/PlayerController.cs
@@ -14,24 +14,24 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        //Get move input
-        var moveInput = Input.GetAxis("Horizontal");
-      
-        //Apply moveSpeed to rigidbody
-        myRigidBody.velocity = new Vector3(moveSpeed * moveInput, myRigidBody.velocity.y, 0);
-        
+        HandleMovement();
+        HandleJump();
+    }
+
+    private void HandleJump()
+    {
         //Get Jump input
         var jumpInput = Input.GetKeyDown(KeyCode.Space);
-        
+
         //check if we're grounded, using a raycast
         //Vector3.down: down in world space.
         //-transform.up: down in the GameObject's local space.
         // var isGrounded = Physics.Raycast(transform.position, Vector3.down, groundCheckDistance);
-        
+
         //Ground checking using sphere cast. Think of it as a sphere being moved along a ray, we hit anything that the sphere touches.
         Ray sphereCastRay = new Ray(transform.position, Vector3.down);
         var isGrounded = Physics.SphereCast(sphereCastRay, groundCheckSphereRadius, groundCheckDistance);
-        
+
         //Draw a ray in the editor, only for visualization.
         Debug.DrawRay(transform.position, Vector3.down * groundCheckDistance, Color.cyan);
 
@@ -39,4 +39,14 @@ public class PlayerController : MonoBehaviour
         if (jumpInput && isGrounded)
             myRigidBody.AddForce(0, jumpForce, 0);
     }
+
+    private void HandleMovement()
+    {
+        //Get move input
+        var moveInput = Input.GetAxis("Horizontal");
+      
+        //Apply moveSpeed to rigidbody
+        myRigidBody.velocity = new Vector3(moveSpeed * moveInput, myRigidBody.velocity.y, 0);
+    }
+    
 }
