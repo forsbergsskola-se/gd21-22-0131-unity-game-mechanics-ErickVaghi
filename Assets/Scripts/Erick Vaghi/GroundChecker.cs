a142051f67e461501fc7379931da6d01186ef5ec
Erick Vaghi <vaghi.erick@gmail.com>
2022-02-04 17:29:20 +0100

    Is grounded is now a separated Script

---
+++ Assets/Scripts/Erick Vaghi/GroundChecker.cs
@@ -0,0 +1,33 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GroundChecker : MonoBehaviour
+{
+    public float groundCheckDistance = 0.6f;
+    
+    public float groundCheckSphereRadius = 0.45f;
+
+    public bool isGrounded;
+
+    private void Update()
+    {
+        CheckIfGrounded();
+    }
+
+    public void CheckIfGrounded()
+    {
+        //check if we're grounded, using a raycast
+        //Vector3.down: down in world space.
+        //-transform.up: down in the GameObject's local space.
+        // var isGrounded = Physics.Raycast(transform.position, Vector3.down, groundCheckDistance);
+        
+        //Ground checking using sphere cast. Think of it as a sphere being moved along a ray, we hit anything that the sphere touches.
+        Ray sphereCastRay = new Ray(transform.position, Vector3.down);
+        isGrounded = Physics.SphereCast(sphereCastRay, groundCheckSphereRadius, groundCheckDistance);
+        
+        //Draw a ray in the editor, only for visualization.
+        //Debug.DrawRay(transform.position, Vector3.down * groundCheckDistance, Color.cyan);
+    }
+}
--- Assets/Scripts/Erick Vaghi/PlayerImmediateJumpController.cs
+++ Assets/Scripts/Erick Vaghi/PlayerImmediateJumpController.cs
@@ -7,11 +7,9 @@ public class PlayerImmediateJumpController : MonoBehaviour
     public Rigidbody myRigidBody;
     
     public float jumpForce = 500f;
-    
-    public float groundCheckDistance = 0.6f;
-    
-    public float groundCheckSphereRadius = 0.45f;
 
+    public GroundChecker myGroundChecker;
+    
     void Update()
     {
         HandleJump();
@@ -20,21 +18,9 @@ public class PlayerImmediateJumpController : MonoBehaviour
     {
         //Get Jump input
         var jumpInput = Input.GetKeyDown(KeyCode.Space);
-
-        //check if we're grounded, using a raycast
-        //Vector3.down: down in world space.
-        //-transform.up: down in the GameObject's local space.
-        // var isGrounded = Physics.Raycast(transform.position, Vector3.down, groundCheckDistance);
-
-        //Ground checking using sphere cast. Think of it as a sphere being moved along a ray, we hit anything that the sphere touches.
-        Ray sphereCastRay = new Ray(transform.position, Vector3.down);
-        var isGrounded = Physics.SphereCast(sphereCastRay, groundCheckSphereRadius, groundCheckDistance);
-
-        //Draw a ray in the editor, only for visualization.
-        Debug.DrawRay(transform.position, Vector3.down * groundCheckDistance, Color.cyan);
-
+        
         //If we pressed the jump button: then jump
-        if (jumpInput && isGrounded)
+        if (jumpInput && myGroundChecker.isGrounded)
             myRigidBody.AddForce(0, jumpForce, 0);
     }
 }
