2cc4e215ce502ae7d27f9d1c4d75b3f047832775
Erick Vaghi <91052540+ErickVaghi@users.noreply.github.com>
2022-02-05 19:27:44 +0100

    Walk and jump input moved to player input controller

--- Assets/Scripts/Erick Vaghi/PlayerImmediateJumpController.cs
+++ Assets/Scripts/Erick Vaghi/PlayerImmediateJumpController.cs
@@ -1,5 +1,3 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerImmediateJumpController : MonoBehaviour
@@ -7,6 +5,8 @@ public class PlayerImmediateJumpController : MonoBehaviour
     public Rigidbody myRigidBody;
     
     public float jumpForce = 500f;
+
+    public PlayerInputController playerInputController;
 
     public GroundChecker myGroundChecker;
     
@@ -16,11 +16,8 @@ public class PlayerImmediateJumpController : MonoBehaviour
     }
     private void HandleJump()
     {
-        //Get Jump input
-        var jumpInput = Input.GetKeyDown(KeyCode.Space);
-        
         //If we pressed the jump button: then jump
-        if (jumpInput && myGroundChecker.isGrounded)
+        if (playerInputController.jumpInput && myGroundChecker.isGrounded)
             myRigidBody.AddForce(0, jumpForce, 0);
     }
 }
--- Assets/Scripts/Erick Vaghi/PlayerInputController.cs
+++ Assets/Scripts/Erick Vaghi/PlayerInputController.cs
@@ -3,6 +3,7 @@ using UnityEngine;
 public class PlayerInputController : MonoBehaviour
 {
     public float walkInput;
+    public bool jumpInput;
     private void Update()
     {
         GetInput();
@@ -11,5 +12,6 @@ public class PlayerInputController : MonoBehaviour
     private void GetInput()
     {
         walkInput = Input.GetAxis("Horizontal");
+        jumpInput = Input.GetKeyDown(KeyCode.Space);
     }
 }
