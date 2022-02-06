a24da3f28072c3a01df0f2053727749e272d7572
Erick Vaghi <91052540+ErickVaghi@users.noreply.github.com>
2022-02-06 20:16:32 +0100

    command controller added to main player prefab

--- Assets/Scripts/Erick Vaghi/PlayerInputController.cs
+++ Assets/Scripts/Erick Vaghi/PlayerInputController.cs
@@ -2,6 +2,7 @@ using UnityEngine;
 
 public class PlayerInputController : MonoBehaviour
 {
+    [SerializeField] private CommandContainer commandContainer;
     public float WalkInput { get; private set; }
     public bool JumpInputDown { get; private set; }
     public bool JumpInput { get; private set; }
@@ -18,5 +19,13 @@ public class PlayerInputController : MonoBehaviour
         JumpInputDown = Input.GetKeyDown(KeyCode.Space);
         JumpInput = Input.GetKey(KeyCode.Space);
         JumpInputUp = Input.GetKeyUp(KeyCode.Space);
+    }
+
+    private void SetCommands()
+    {
+        commandContainer.walkCommand = WalkInput;
+        commandContainer.jumpCommand = JumpInput;
+        commandContainer.jumpCommandDown = JumpInputDown;
+        commandContainer.jumpCommandUp = JumpInputUp;
     }
 }
