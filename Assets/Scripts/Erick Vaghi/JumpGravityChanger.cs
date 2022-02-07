522d3e43452af2e553b5582abe9f2fc9a05dd01a
Erick Vaghi <91052540+ErickVaghi@users.noreply.github.com>
2022-02-07 20:07:51 +0100

    improved jump Script (involving gravity)

---
+++ Assets/Scripts/Erick Vaghi/JumpGravityChanger.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+
+public class JumpGravityChanger : MonoBehaviour
+{
+    [SerializeField] private float fallMultiplier = 2.5f;
+    [SerializeField] private float lowJumpMultiplier = 2f;
+    
+    [SerializeField] private Rigidbody myRigidbody;
+    [SerializeField] private CommandContainer commandContainer;
+    void Update()
+    {
+        ImprovedJump();
+    }
+
+    void ImprovedJump()
+    {
+        if (myRigidbody.velocity.y < 0)
+        {
+            myRigidbody.velocity += Vector3.up * Physics.gravity.y * (fallMultiplier - 1) * Time.deltaTime;
+        }
+        else if (myRigidbody.velocity.y > 0 && commandContainer.jumpCommand)
+        {
+            myRigidbody.velocity += Vector3.up * Physics.gravity.y * (lowJumpMultiplier - 1) * Time.deltaTime;
+        }
+    }
+}
