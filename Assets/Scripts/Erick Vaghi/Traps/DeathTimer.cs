52cbc1cc39562c38a6a994677c3d762df58ee7b3
Erick Vaghi <vaghi.erick@gmail.com>
2022-02-09 21:46:44 +0100

    Level design 1

--- Assets/Scripts/Erick Vaghi/Traps/DeathTimer.cs
+++ Assets/Scripts/Erick Vaghi/Traps/DeathTimer.cs
@@ -1,3 +1,4 @@
+using System;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
@@ -6,15 +7,29 @@ public class DeathTimer : MonoBehaviour
     [SerializeField] private float deathTimeCounter;
     [SerializeField] private float deathTime = 10f;
     [SerializeField] private GameObject player;
+    [SerializeField] private GameObject deathParticles;
+    private bool deathAnimation = false;
+
+    private void Awake()
+    {
+        deathParticles.SetActive(false);
+    }
 
     private void Start()
     {
+        deathAnimation = false;
         deathTimeCounter = deathTime;
     }
 
     public void Update()
     {
-        Destroy(player);
+        if (!deathAnimation)
+        {
+            deathParticles.transform.position = player.transform.position;
+            deathParticles.SetActive(true);
+            Destroy(player);
+            deathAnimation = true;
+        }
         deathTimeCounter -= Time.deltaTime;
         if (deathTimeCounter < 0)
         {
