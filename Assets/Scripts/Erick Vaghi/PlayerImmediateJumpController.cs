d5b9833301d38b93479e2dcb4c1b3164f1753489
Erick Vaghi <vaghi.erick@gmail.com>
2022-02-09 23:06:29 +0100

    added goal Prefab

--- Assets/Scripts/Erick Vaghi/PlayerImmediateJumpController.cs
+++ Assets/Scripts/Erick Vaghi/PlayerImmediateJumpController.cs
@@ -1,7 +1,10 @@
+using System;
 using UnityEngine;
 
 public class PlayerImmediateJumpController : MonoBehaviour
 {
+    [SerializeField] private Animator animator;
+    
     [SerializeField] private Rigidbody myRigidBody;
     
     [SerializeField] private float jumpForce = 500f;
@@ -9,15 +12,33 @@ public class PlayerImmediateJumpController : MonoBehaviour
     [SerializeField] private CommandContainer commandContainer;
 
     [SerializeField] private GroundChecker myGroundChecker;
-    
+
     void Update()
     {
         HandleJump();
+        if (myGroundChecker.IsGrounded)
+        {
+            animator.ResetTrigger("Player is Jumping");
+            animator.SetTrigger("Player Stopped Jumping");
+
+        }
+        else
+        {
+            animator.SetTrigger("Player is Jumping");
+            animator.ResetTrigger("Player Stopped Jumping");
+        }
     }
     private void HandleJump()
     {
         //If we pressed the jump button: then jump
         if (commandContainer.jumpCommandDown && myGroundChecker.IsGrounded)
+        {
             myRigidBody.AddForce(0, jumpForce, 0);
+
+        }
+        else if(myGroundChecker.IsGrounded)
+        {
+
+        }
     }
 }
