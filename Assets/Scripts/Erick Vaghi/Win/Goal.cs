f56612f28b41a40ae4740855f5bdea128cde1f67
Erick Vaghi <vaghi.erick@gmail.com>
2022-02-09 23:22:55 +0100

    Improved jump and created new level

--- Assets/Scripts/Erick Vaghi/Win/Goal.cs
+++ Assets/Scripts/Erick Vaghi/Win/Goal.cs
@@ -1,18 +1,11 @@
-using System.Collections;
-using System.Collections.Generic;
+using System;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Goal : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private void OnTriggerEnter(Collider other)
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex+1);
     }
 }
